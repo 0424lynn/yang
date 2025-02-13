60988412ae21c1933bfc674924ff568107c1c811
0424lynn <chris@smartkitchenservice.com>
2025-02-12 20:35:29 -0800

    添加 dashboard.html

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault();  // 阻止默认提交，防止 405 错误
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // 假设用户名是 "admin"，密码是 "1234"
+  if (username === "admin" && password === "1234") {
+      alert("登录成功！");
+      window.location.href = "pages/dashboard.html";  // 成功后跳转
+  } else {
+      alert("用户名或密码错误，请重试！");
+  }
+});
