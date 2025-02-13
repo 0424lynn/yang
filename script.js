a442ea3fd654451fc7fdca1a5befa3d4dcffc3d6
0424lynn <chris@smartkitchenservice.com>
2025-02-12 20:41:57 -0800

    确保 dashboard.html 上传

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   // 假设用户名是 "admin"，密码是 "1234"
   if (username === "admin" && password === "1234") {
       alert("登录成功！");
-      window.location.href = "pages/dashboard.html";  // 成功后跳转
+      window.location.href = "dashboard.html";  // 成功后跳转
   } else {
       alert("用户名或密码错误，请重试！");
   }
